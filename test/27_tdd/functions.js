5833c5e71d208cdc68e9dfce910e2ba475697eb8
Yasunari_Kainuma <yasu6491@icloud.com>
2022-02-23 20:26:15 +0900

    add: add関数とテスト実装

--- test/27_tdd/functions.js
+++ test/27_tdd/functions.js
@@ -1,4 +1,34 @@
-function sum(a, b) {
-  return a + b;
+const argsEmpCheck = (args) => {
+  if (args.length === 0) {
+    throw new Error('Arguments should be number.');
+  }
 }
-module.exports = sum;
+
+const argsLengthCheck = (args) => {
+  if (args.length > 30) {
+    throw new Error('Arguments should be less than 30.');
+  }
+};
+
+const argsTypeCheck = (args) => {
+  if (args.some((arg) => typeof arg === 'string')) {
+    throw new Error('Arguments should be more than 0.');
+  }
+}
+
+
+export const add = (numbers) => {
+  argsEmpCheck(numbers);
+  argsLengthCheck(numbers);
+  argsTypeCheck(numbers);
+
+  const result = numbers.reduce(
+    ( previousValue, currentValue ) => previousValue + currentValue,
+    0
+  )
+
+  if (result > 1000) {
+    return 'too big';
+  }
+  return result;
+}
--- test/27_tdd/functions.test.js
+++ test/27_tdd/functions.test.js
@@ -1,5 +1,37 @@
-const sum = require('./functions');
+import { add } from './functions';
 
-test('adds 1 + 2 to equal 3', () => {
-  expect(sum(1, 2)).toBe(3);
-});
+describe('add', () => {
+  describe('正常系', () => {
+    it('[3, 10, 3]を受け取った場合、16が返ること', () => {
+      const args = [3, 10, 3]
+      expect(add(args)).toBe(16);
+    })
+
+    it('30個の引数を受け取った場合、和が返ること', () => {
+      const args = Array(30).fill(1);
+      expect(add(args)).toBe(30);
+    })
+
+    it('計算結果が1000を超える場合、「too big」と文字列が返ること', () => {
+      const args = [1000, 1]
+      expect(add(args)).toBe('too big');
+    })
+  })
+
+  describe('異常系', () => {
+    it('数字以外の引数を受け取った場合、エラーが発生すること', () => {
+      const args = ['test']
+      expect(() => add(args)).toThrow();
+    })
+
+    it('引数が0個だった場合、エラーが発生すること', () => {
+      const args = []
+      expect(() => add(args)).toThrow();
+    })
+
+    it('31個以上の引数を受け取った場合、エラーが発生すること', () => {
+      const args = Array(31).fill(1);
+      expect(() => add(args)).toThrow();
+    })
+  })
+})
